04be7f4ef86b88a40ae9aa8c662e2583517627ec
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2020-11-29 18:26:29 -0600

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+module.exports = {
+    colors: {
+        info: '#5b57d9',
+        error: '#de554e',
+        neutral: '#f2f255',
+        success: '#43d177'
+    },
+    firedRank: 1,
+    xpRankup: {
+        enabled: false,
+        roles: [
+            // {
+            //     rank: 2,
+            //     xpNeeded: 10
+            // }
+        ]
+    }
+}
