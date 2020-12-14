19ae8b646c499fdc31c6700ef4292454320c2ed6
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2020-12-13 19:02:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -130,22 +130,24 @@ client.on('message', async (message) => {
             return message.channel.send(embed);
         }
     }
-
-    if(!cooldowns.has(command.name)) {
-        cooldowns.set(command.name, new Discord.Collection());
-    }
-    let currentDate = Date.now();
-    let userCooldowns = cooldowns.get(command.name);
-    let cooldownAmount = (command.cooldown || 3) * 1000;
-    if(userCooldowns.has(message.author.id)) {
-        let expirationDate = timestamps.get(message.author.id) + cooldownAmount;
-        if(currentDate < expirationDate) {
-            let timeLeft = (expirationTime - currentDate) / 1000;
-            let embed = new Discord.MessageEmbed();
-            embed.setDescription(`This command is currently on cooldown. Please try again in ${timeLeft.toString()}`);
-            embed.setColor(client.constants.colors.error);
-            embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
-            return message.channel.send(embed);
+    
+    if(command.cooldown) {
+        if(!cooldowns.has(command.name)) {
+            cooldowns.set(command.name, new Discord.Collection());
+        }
+        let currentDate = Date.now();
+        let userCooldowns = cooldowns.get(command.name);
+        let cooldownAmount = (command.cooldown || 3) * 1000;
+        if(userCooldowns.has(message.author.id)) {
+            let expirationDate = timestamps.get(message.author.id) + cooldownAmount;
+            if(currentDate < expirationDate) {
+                let timeLeft = (expirationTime - currentDate) / 1000;
+                let embed = new Discord.MessageEmbed();
+                embed.setDescription(`This command is currently on cooldown. Please try again in ${timeLeft.toString()}`);
+                embed.setColor(client.constants.colors.error);
+                embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
+                return message.channel.send(embed);
+            }
         }
     }
 
