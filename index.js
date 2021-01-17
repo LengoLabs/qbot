e577890a71a876b6df17c77394fce8d385c25ed2
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2021-01-17 12:40:45 -0600

    Add member count logging & new config system

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.get('/', (request, response) => {
     response.sendStatus(200);
 });
 
-const listener = app.listen(process.env.PORT, () => {
+const listener = app.listen(client.config.PORT, () => {
     console.log('Your app is currently listening on port: ' + listener.address().port);
 });
 
@@ -15,33 +15,40 @@ const roblox = require('noblox.js');
 const chalk = require('chalk');
 const figlet = require('figlet');
 const fs = require('fs');
-const constants = require('./constants.js');
+const fetch = require('node-fetch');
+const config = require('./config.js');
 const utils = require('./utils.js');
 require('dotenv').config();
 
 roblox.setCookie(process.env.cookie).then((botAccount) => {
-    roblox.onAuditLog(process.env.groupId).on('data', async (data) => {
-        let auditLogChannel = await client.channels.fetch(process.env.auditLogChannelId);
+    roblox.onAuditLog(client.config.groupId).on('data', async (data) => {
+        let auditLogChannel = await client.channels.fetch(client.config.auditLogChannelId);
         if(data.actor.user.userId === botAccount.UserID) return;
         let embed = new Discord.MessageEmbed();
         if(data.actionType === 'Change Rank') {
-            let roles = await roblox.getRoles(process.env.groupId);
+            let roles = await roblox.getRoles(client.config.groupId);
             data.description.OldRoleSetRank = (roles.find(r => r.ID === data.description.OldRoleSetId)).rank;
             data.description.NewRoleSetRank = (roles.find(r => r.ID === data.description.NewRoleSetId)).rank;
             embed.setAuthor(data.actor.user.displayName, `https://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&format=png&username=${data.actor.user.username}`);
             embed.setDescription(`**Moderator:** ${data.actor.user.displayName} (\`${data.actor.user.userId}\`)\n**Action:** Manual Ranking\n**User:** ${data.description.TargetName} (\`${data.description.TargetId}\`)\n**Rank Change:** ${data.description.OldRoleSetName} (${data.description.OldRoleSetRank}) -> ${data.description.NewRoleSetName} (${data.description.NewRoleSetRank})`);
             embed.setThumbnail(`https://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&format=png&username=${data.description.TargetName}`);
             embed.setTimestamp();
-            embed.setColor(client.constants.colors.info);
+            embed.setColor(client.config.colors.info);
             return auditLogChannel.send(embed);
         }
         if(data.actionType === 'Post Status') {
             embed.setAuthor(data.actor.user.displayName, `https://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&format=png&username=${data.actor.user.username}`);
-            embed.setDescription(`**Poster:** ${data.actor.user.displayName} (\`${data.actor.user.userId}\`)\n**Action:** Manual Shout\n**Message:**\n\`\`\`${data.description.Text}\`\`\``);
+            embed.setDescription(`**Poster:** ${data.actor.user.displayName} (\`${data.actor.user.userId}\`)\n${
+                data.description.Text !== '' ? `**Action:** Manual Shout\n**Message:**\n\`\`\`${data.description.Text}\`\`\`` : `**Action:** Manual Clear Shout`
+            }`);
             embed.setTimestamp();
-            embed.setColor(client.constants.colors.info);
+            embed.setColor(client.config.colors.info);
             return auditLogChannel.send(embed);
         }
+    });
+
+    roblox.onAuditLog(client.config.groupId).on('error', (err) => {
+        console.log(`Error with group audit log integration: ${err}`);
     });
 }).catch(async err => {
     console.log(chalk.red('Issue with logging in: ' + err));
@@ -49,7 +56,7 @@ roblox.setCookie(process.env.cookie).then((botAccount) => {
 
 let commandList = [];
 client.commandList = commandList;
-client.constants = constants;
+client.config = config;
 client.utils = utils;
 client.databases = {};
 
@@ -74,32 +81,68 @@ let shout;
 
 const onShout = async () => {
     let embed = new Discord.MessageEmbed();
-    let shoutChannel = await client.channels.cache.get(process.env.shoutChannelId);
+    let shoutChannel = await client.channels.cache.get(client.config.shoutChannelId);
     if(firstShout == true){
         firstShout = false;
-        shout = await roblox.getShout(Number(process.env.groupId));
+        shout = await roblox.getShout(client.config.groupId);
         setTimeout(onShout, 30000);
     } else {
         setTimeout(onShout, 30000);
-        let currentShout = await roblox.getShout(Number(process.env.groupId));
+        let currentShout = await roblox.getShout(client.config.groupId);
         if(currentShout.body == shout.body) return;
         if(currentShout.body){
             embed.setDescription(`${currentShout.body}`);
-            embed.setColor(client.constants.colors.info);
+            embed.setColor(client.config.colors.info);
             embed.setAuthor(currentShout.poster.username, `https://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&format=png&username=${shout.poster.username}`);
             shoutChannel.send(embed);
         } else {
             embed.setDescription(`*Shout cleared.*`);
-            embed.setColor(client.constants.colors.info);
+            embed.setColor(client.config.colors.info);
             embed.setAuthor(currentShout.poster.username, `https://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&format=png&username=${shout.poster.username}`);
             shoutChannel.send(embed);
         }
         shout = currentShout;
     }
 }
-if(process.env.shoutChannelId !== 'false'){
+if(client.config.shoutChannelId !== 'false'){
     setTimeout(onShout, 15000);
 }
+
+let currentMemberCount = 0;
+let firstCheck = true;
+
+let refreshCount = async () => {
+    let channel = await client.channels.fetch(client.config.memberCount.channelId);
+    let groupResponse = await fetch(`https://groups.roblox.com/v1/groups/${client.config.groupId}`);
+    let groupBody = await groupResponse.json();
+    let newCount = groupBody.memberCount;
+    if(firstCheck === true) {
+        firstCheck = false;
+        currentMemberCount = newCount;
+        return setTimeout(refreshCount, 30000);
+    }
+    if(client.config.memberCount.milestones.some(milestone => newCount > milestone && currentMemberCount < milestone)) {
+        let milestoneReached = client.config.memberCount.milestones.find(milestone => newCount > milestone && currentMemberCount < milestone);
+        let embed = new Discord.MessageEmbed();
+        embed.setAuthor(groupBody.name, client.config.memberCount.groupIconURL);
+        embed.setTitle('🎉 Milestone Reached!');
+        embed.setDescription(`${groupBody.name} just hit the ${milestoneReached} group member count milestone!`);
+        embed.setColor(client.config.colors.success);
+        return channel.send(embed);
+    }
+    if(newCount !== currentMemberCount) {
+        if(newCount > currentMemberCount) {
+            channel.send(`⬆️ The group member count has increased! It is now at ${newCount}.`);
+        }
+        if(newCount < currentMemberCount) {
+            channel.send(`⬇️ The group member count has decreased! It is now at ${newCount}.`);
+        }
+    }
+    currentMemberCount = newCount;
+    setTimeout(refreshCount, 30000);
+}
+
+if(client.config.memberCount.enabled) refreshCount();
 
 fs.readdir('./commands/', async (err, files) => {
     if(err){
@@ -117,7 +160,7 @@ fs.readdir('./commands/', async (err, files) => {
 });
 
 client.on('ready', async () => {
-    console.log(`${chalk.hex(client.constants.colors.info)(figlet.textSync('qbot', { horizontalLayout: 'full' }))}\n`);
+    console.log(`${chalk.hex(client.config.colors.info)(figlet.textSync('qbot', { horizontalLayout: 'full' }))}\n`);
     console.log(`${chalk.hex('#60bf85')('Bot started!')}\n⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n`
     + `${chalk.hex('#ffaa2b')('>')} ${chalk.hex('#7289DA')(`Servers: ${chalk.hex('#4e5f99')(`${client.guilds.cache.size}`)}`)}\n`
     + `${chalk.hex('#ffaa2b')('>')} ${chalk.hex('#7289DA')(`Channels: ${chalk.hex('#4e5f99')(`${client.channels.cache.size}`)}`)}`);
@@ -138,8 +181,8 @@ client.on('ready', async () => {
 
 client.on('message', async (message) => {
     if(message.author.bot) return;
-    if(!message.content.startsWith(process.env.prefix)) return;
-    const args = message.content.slice(process.env.prefix.length).split(' ');
+    if(!message.content.startsWith(client.config.prefix)) return;
+    const args = message.content.slice(client.config.prefix.length).split(' ');
     const commandName = args[0].toLowerCase();
     args.shift();
     const command = commandList.find((cmd) => cmd.name === commandName || cmd.config.aliases.includes(commandName));
@@ -149,7 +192,7 @@ client.on('message', async (message) => {
         if(!message.member.roles.cache.some(role => command.config.rolesRequired.includes(role.name))) {
             let embed = new Discord.MessageEmbed();
             embed.setDescription('You do not have permission to use this command.');
-            embed.setColor(client.constants.colors.error);
+            embed.setColor(client.config.colors.error);
             embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
             return message.channel.send(embed);
         }
@@ -168,7 +211,7 @@ client.on('message', async (message) => {
                 let timeLeft = Math.round((expirationDate - currentDate) / 1000);
                 let embed = new Discord.MessageEmbed();
                 embed.setDescription(`This command is currently on cooldown. Please try again in ${timeLeft.toString()} seconds.`);
-                embed.setColor(client.constants.colors.error);
+                embed.setColor(client.config.colors.error);
                 embed.setAuthor(message.author.tag, message.author.displayAvatarURL());
                 return message.channel.send(embed);
             } else {
