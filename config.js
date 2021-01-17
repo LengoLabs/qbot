9cc3c41114865fde842eced88213bcf474a44997
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2021-01-17 12:37:49 -0600

    Create new config format

---
+++ config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    groupId: 0,
+    maximumRank: 0,
+    prefix: 'q!',
+    logChannelId: '',
+    shoutChannelId: '',
+    auditLogChannelId: '',
+    colors: {
+        info: '#5b57d9',
+        error: '#de554e',
+        neutral: '#f2f255',
+        success: '#43d177'
+    },
+    firedRank: 1,
+    xpRankup: {
+        enabled: false,
+        roles: []
+    },
+    memberCount: {
+        enabled: false,
+        groupIconURL: '',
+        channelId: '',
+        milestones: []
+    }
+}
