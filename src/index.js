53076c1de920c9a71548d4be0da29015c5dea85a
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-08-20 15:01:11 -0500

    fix intents

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client({
     allowedMentions: { parse: [] },
     intents: [
-        Discord.FLAGS.GUILD_MESSAGES,
-        Discord.FLAGS.GUILD_MESSAGE_REACTIONS
+        Discord.Intents.FLAGS.GUILD_MESSAGES,
+        Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS
     ]
 });
 const roblox = require('noblox.js');
