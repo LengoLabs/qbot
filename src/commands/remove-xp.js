4946e7ecbdc08185f5b32241299963476db8fa7b
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-10-20 21:06:36 -0500

    Fix v13 support for remove-xp command

--- src/commands/remove-xp.js
+++ src/commands/remove-xp.js
@@ -230,7 +230,7 @@ module.exports = {
         embed.setDescription(`Removed \`${decrement}xp\` from ${displayUsername}'s XP count. They now have \`${afterChangeXP}xp\`.`);
         embed.setColor(client.config.colors.success);
         embed.setAuthor(interaction.user.tag, interaction.user.displayAvatarURL());
-        interaction.reply(embed);
+        interaction.reply({ embeds: [embed] });
 
         if(client.config.logChannelId !== 'false') {
             let logEmbed = new Discord.MessageEmbed();
