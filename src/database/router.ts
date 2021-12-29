836b9362d866d7bb21c3763a810243c1ba30c12f
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2021-12-28 19:33:00 -0600

    Update router.ts

--- src/database/router.ts
+++ src/database/router.ts
@@ -1,15 +1,7 @@
 import { DatabaseProvider } from '../structures/DatabaseProvider';
 import { MongoDBProvider } from './mongodb';
-import { SQLiteProvider } from './sqlite';
 import { config } from '../config';
 
-let provider: MongoDBProvider | SQLiteProvider;
-if(config.database.enabled) {
-    if(config.database.type === 'mongodb') {
-        provider = new MongoDBProvider();
-    } else {
-        provider = new SQLiteProvider();
-    }
-}
+let provider = new MongoDBProvider();
 
 export { provider };
