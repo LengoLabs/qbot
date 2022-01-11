3c2abfd6b12643b806cb8e81dcc487b2379a3411
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2022-01-10 21:30:45 -0600

    Update suspend.ts

--- src/commands/suspensions/suspend.ts
+++ src/commands/suspensions/suspend.ts
@@ -93,6 +93,7 @@ class SuspendCommand extends Command {
         let duration: number;
         try {
             duration = Number(ms(ctx.args['duration']));
+            if(!duration) throw new Error();
             if(duration < 0.5 * 60000 && duration > 6.31138519 * (10 ^ 10) ) return ctx.reply({ embeds: [ getInvalidDurationEmbed() ] });
         } catch (err) {
             return ctx.reply({ embeds: [ getInvalidDurationEmbed() ] });
