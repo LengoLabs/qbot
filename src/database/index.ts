82508fa37206d67b8c47e9e622f0a3854cea21c8
wilyt1 <63251107+wilyt1@users.noreply.github.com>
2022-01-16 08:18:35 -0500

    Remove SQLiteProvider

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,6 @@
 import { MongoDBProvider } from './mongodb';
-import { SQLiteProvider } from './sqlite';
 import { config } from '../config';
 
-const provider = config.database.enabled ? (config.database.type === 'mongodb' ? MongoDBProvider : SQLiteProvider) : null;
+const provider = config.database.enabled ? MongoDBProvider : null;
 
 export { provider };
