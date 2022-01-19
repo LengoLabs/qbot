92f02445b19fc0c1abb5bc11bb2b17c600a72dd2
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2022-01-19 23:19:25 +0000

    Fix provider system

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import { MongoDBProvider } from './mongodb';
 import { config } from '../config';
 
-const provider = config.database.enabled ? (config.database.type === 'mongodb' ? MongoDBProvider : SQLiteProvider) : null;
+const provider = config.database.enabled ? (config.database.type === 'mongodb' ? MongoDBProvider : null) : null;
 
 export { provider };
