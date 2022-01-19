c7d9c570a1daf70f085ee07b79a983b3aacc6c48
Lengo <28582329+yogurtsyum@users.noreply.github.com>
2022-01-19 23:18:26 +0000

    Fix SQLiteProvider import

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,5 +1,4 @@
 import { MongoDBProvider } from './mongodb';
-import { SQLiteProvider } from './sqlite';
 import { config } from '../config';
 
 const provider = config.database.enabled ? (config.database.type === 'mongodb' ? MongoDBProvider : SQLiteProvider) : null;
