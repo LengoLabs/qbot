92f4319d774550b48c4918bc80e51147dc03f386
UnknownPerson3420 <76407758+UnknownPerson3420@users.noreply.github.com>
2022-01-26 13:36:07 -0500

    Update locale.ts
    Sends reason in /info response.

--- src/handlers/locale.ts
+++ src/handlers/locale.ts
@@ -451,6 +451,7 @@ export const getPartialUserInfoEmbed = async (user: User | PartialUser, data: Da
         .setTimestamp()
         .addField('Role', 'Guest (0)', true)
         .addField('Banned', data.isBanned ? `✅` : '❌', true)
+        .addField('Reason', `${data.reasonForBan}` || `N/A`, true)
 
     return embed;
 }
@@ -468,7 +469,8 @@ export const getUserInfoEmbed = async (user: User | PartialUser, member: GroupMe
         .addField('XP', data.xp.toString() || '0', true)
         .addField('Suspended', data.suspendedUntil ? `✅ (<t:${Math.round(data.suspendedUntil.getTime() / 1000)}:R>)` : '❌', true)
         .addField('Banned', data.isBanned ? `✅` : '❌', true);
-
+        .addField('Reason', `${data.reasonForBan}` || `N/A`, true)
+    
     return embed;
 }
 
