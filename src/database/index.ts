226e260f8e04a7736fe68f1c21ed7016df2c308d
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2022-12-24 20:10:15 -0600

    Remove all files for CF Worker update

--- src/database/index.ts
+++
@@ -1,6 +0,0 @@
-import { MongoDBProvider } from './mongodb';
-import { config } from '../config';
-
-const provider = config.database.enabled ? (config.database.type === 'mongodb' ? MongoDBProvider : null) : null;
-
-export { provider };
--- src/structures/DatabaseProvider.ts
+++
@@ -1,7 +0,0 @@
-abstract class DatabaseProvider {
-    abstract findUser(query: any);
-    abstract findSuspendedUsers();
-    abstract updateUser(query: any, data: any);
-}
-
-export { DatabaseProvider };
