bcafbdb11445198d5f8260a457a2bfd070ab1bb2
yogurtsyum <28582329+yogurtsyum@users.noreply.github.com>
2023-07-22 14:31:43 -0500

    autocomplete type fix again?

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -33,8 +33,7 @@ const mapArgument = (arg: CommandArgument) => {
         name: arg.trigger,
         description: arg.description || 'No description provided.',
         type: argumentTypeMappings[arg.type],
-        // @ts-ignore
-        autocomplete: arg.autocomplete || false,
+        autocomplete: arg.autocomplete,
         required: arg.required !== null && arg.required !== undefined ? arg.required : true,
         choices: arg.choices || [],
         options: arg.args ? arg.args.map(mapArgument) : [],
